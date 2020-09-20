806687140eed09ff386fbd4b0f9da9175690cbee
Kapil Pathak <pathak.kapil@btech2013.iitgn.ac.in>
2020-09-20 11:38:34 +0530

    Create DP_Longest_Common_Substring.py

---
+++ DP_Longest_Common_Substring.py
@@ -0,0 +1,41 @@
+#code
+"""
+Given two strings X and Y. The task is to find the length of the longest common substring.
+
+Input:
+First line of the input contains number of test cases T. Each test case consist of three lines, first of which contains 2 space separated integers N and M denoting the size of string X and Y strings respectively. The next two lines contains the string X and Y.
+
+Output:
+For each test case print the length of longest  common substring of the two strings .
+"""
+
+
+def LCSubStr(X, Y, m, n): 
+
+    LCSuff = [[0 for k in range(n+1)] for l in range(m+1)] 
+      
+    # To store the length of  
+    # longest common substring 
+    result = 0 
+  
+    # Following steps to build 
+    # LCSuff[m+1][n+1] in bottom up fashion 
+    for i in range(m + 1): 
+        for j in range(n + 1): 
+            if (i == 0 or j == 0): 
+                LCSuff[i][j] = 0
+            elif (X[i-1] == Y[j-1]): 
+                LCSuff[i][j] = LCSuff[i-1][j-1] + 1
+                result = max(result, LCSuff[i][j]) 
+            else: 
+                LCSuff[i][j] = 0
+    return result
+n1 = int(input())
+for i in range(n1):
+    arr = list(map(int, input().split()))  
+    str1 = input()
+    str2 = input()
+    m = arr[0]
+    n = arr[1]
+    print(LCSubStr(str1, str2, m, n))
+    
